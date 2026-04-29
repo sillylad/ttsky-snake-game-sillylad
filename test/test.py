e5c1dfb44ad10b23b1ce3c29a345644f5f7e71ba
lilly <ldas@andrew.cmu.edu>
2026-04-28 23:59:27 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -28,36 +28,60 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
     
-    snek = dut.user_project.ci.snek
-
-    # Set the input values you want to test
-    
+    # change test based on gate level or rtl sim since post-synth can't use
+    # the internal signals needed for the functional test (??)
+    try:
+        snek = dut.user_project.ci.snek
+        gl_test = False
+        dut._log.info("hierarchical signals usable")
+    except AttributeError:
+        gl_test = True
+        dut._log.info("only ports")
+        
     # super simple test to simulate moving the snake forwards from the init state
+    if not gl_test:    
+        # hit the start game button
+        print(snek.curr_state.value)
+        dut.ui_in.value = 0b000_0000_1
+        await ClockCycles(dut.clk, 5) # propagate btn input into dut
 
-    # hit the start game button
-    print(snek.curr_state.value)
-    dut.ui_in.value = 0b000_0000_1
-    await ClockCycles(dut.clk, 5) # propagate btn input into dut
+        print(snek.start_game.value)
+        
+        # wait for game_clk so start_game gets used
+        while True:
+            print("waiting for game_clk posedge")
+            await RisingEdge(snek.game_clk)
+            await ReadOnly()
+            if(dut.user_project.ci.start_game.value == 1):
+                print("both game_clk and start_game high, proceeding")
+                break
+        
+        await ClockCycles(dut.clk, 5)
+        print(f"curr_state: {snek.curr_state.value}")
+        print(f"start_game: {dut.user_project.ci.start_game.value}")
+        assert dut.user_project.ci.start_game.value == 1
+        print(f"game_clk: {snek.game_clk.value}")
+        # assert that the snake moved out of the idle state
+        assert snek.curr_state.value != 0
+    
+    # even more basic test for gate-level sim
+    # just checking that the vga is ... alive
+    else:
+        await ClockCycles(dut.clk, 10000000)
+        hs_seen = False
+        
+        # hs period is like 800 so this should be enough time...
+        for i in range(1000):
+            await ClockCycles(dut.clk, 100)
+            if dut.uo_out.value & 0b10:  # VGA_HS on uo[1]
+                hs_seen = True
+                break
+        
+        assert hs_seen
+        
+        
 
-    print(snek.start_game.value)
-    
-    # wait for game_clk so start_game gets used
-    while True:
-        print("waiting for game_clk posedge")
-        await RisingEdge(snek.game_clk)
-        await ReadOnly()
-        if(dut.user_project.ci.start_game.value == 1):
-            print("both game_clk and start_game high, proceeding")
-            break
-    
-    await ClockCycles(dut.clk, 5)
-    print(f"curr_state: {snek.curr_state.value}")
-    print(f"start_game: {dut.user_project.ci.start_game.value}")
-    assert dut.user_project.ci.start_game.value == 1
-    print(f"game_clk: {snek.game_clk.value}")
-    # assert that the snake moved out of the idle state
-    assert snek.curr_state.value != 0
-    
+    ##  Testbenches for when you want to sit around forever :) ## 
     # wait for snake to die (takes a while since game_clk takes forever...)
     # game_clk_cnt = 0
     # while True:
